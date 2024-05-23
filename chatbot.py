9f992874585175814c6c131888ba8ec07dc7d6b1
MelvinCZJ <77262225+MelvinCZJ@users.noreply.github.com>
2024-05-23 15:11:45 +0800

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,56 @@
+import ollama
+import streamlit as st
+import shelve
+
+st.title("Streamlit Chatbot Interface")
+
+USER_AVATAR = "👤"
+BOT_AVATAR = "🤖"
+
+# Load chat history from shelve file
+def load_chat_history():
+    with shelve.open("chat_history") as db:
+        return db.get("messages", [])
+
+
+# Save chat history to shelve file
+def save_chat_history(messages):
+    with shelve.open("chat_history") as db:
+        db["messages"] = messages
+
+
+# Initialize or load chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = load_chat_history()
+
+# Sidebar with a button to delete chat history
+with st.sidebar:
+    if st.button("Delete Chat History"):
+        st.session_state.messages = []
+        save_chat_history([])
+
+# Display chat messages
+for msg in st.session_state.messages:
+    if msg["role"] == "user":
+        st.chat_message(msg["role"], avatar="🧑‍💻").write(msg["content"])
+    else:
+        st.chat_message(msg["role"], avatar="🤖").write(msg["content"])
+
+## Generator for Streaming Tokens
+def generate_response():
+    response = ollama.chat(model='llama2', stream=True, messages=st.session_state.messages)
+    for partial_resp in response:
+        token = partial_resp["message"]["content"]
+        st.session_state["full_message"] += token
+        yield token
+
+if prompt := st.chat_input("How can I help?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user", avatar="🧑‍💻").write(prompt)
+    st.session_state["full_message"] = ""
+    st.chat_message("assistant", avatar="🤖").write_stream(generate_response)
+    st.session_state.messages.append({"role": "assistant", "content": st.session_state["full_message"]})   
+
+
+# Save chat history after each interaction
+save_chat_history(st.session_state.messages)
